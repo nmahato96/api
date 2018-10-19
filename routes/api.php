eb24bb4d591ef17bca35b24249235494387be922
Niladri Mahato <niladrimahato96@gmail.com>
2018-10-19 12:31:01 +0530

    092-implementing-the-index-method-for-categoryproductcontroller

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,13 @@ Route::resource('buyers', 'Buyer\BuyerController',['only' => ['index','show']]);
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController',['only' => ['index']]);
 Route::resource('buyers.products', 'Buyer\BuyerProductController',['only' => ['index']]);
 Route::resource('buyers.sellers', 'Buyer\BuyerSellerController',['only' => ['index']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController',['only' => ['index']]);
 
 /*
  * Categories
  */
 Route::resource('categories', 'Category\CategoryController',['except' => ['create','edit']]);
+Route::resource('categories.products', 'Category\CategoryProductController',['only' => ['index']]);
 
 /*
  * Products
